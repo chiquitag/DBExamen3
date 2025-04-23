a98977d3e3eccb9b708565357c5a751a4f8b2483
Sonistwo <159084179+Sonistwo@users.noreply.github.com>
2025-04-22 23:03:12 -0500

    Se añadieron las clases para generación y validación de tokens y se ajustaron las configuraciones

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -13,7 +13,7 @@ namespace DBExamen3
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-
+            config.MessageHandlers.Add(new TokenValidationHandler());
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
