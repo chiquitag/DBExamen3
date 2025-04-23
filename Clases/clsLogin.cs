87669c6f6f4bc244e2903dadfe314a98fe5b2aea
Sonistwo <159084179+Sonistwo@users.noreply.github.com>
2025-04-22 23:16:38 -0500

    Se crearon las clases modelo Login y LoginRespuesta y se ajustó la clsLogin para no tomar en cuenta encriptación.

---
+++ Clases/clsLogin.cs
@@ -0,0 +1,59 @@
+﻿
+using DBExamen3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DBExamen3.Clases
+{
+    public class clsLogin
+    {
+        private DBExamenEntities dbSuper = new DBExamenEntities();
+        public Login login { get; set; }
+        private LoginRespuesta logRpta;
+        private bool ValidarUsuario()
+        {
+            try
+            {
+                AdministradorITM usuario = dbSuper.AdministradorITMs.FirstOrDefault(u => u.Usuario == login.Usuario);
+                if (usuario == null)
+                {
+                    logRpta = new LoginRespuesta();
+                    logRpta.Mensaje = "Usuario no existe";
+                    return false;
+                }
+                login.Clave = usuario.Clave;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                logRpta = new LoginRespuesta();
+                logRpta.Mensaje = ex.Message;
+                return false;
+            }
+        }
+        public IQueryable<LoginRespuesta> Ingresar()
+        {
+            if (ValidarUsuario())
+            {
+                //string token = TokenGenerator.GenerateTokenJwt(login.Usuario);
+                string token = TokenGenerator.GenerateTokenJwt(login.Usuario);
+                return from U in dbSuper.Set<AdministradorITM>()
+                       where U.Usuario == login.Usuario &&
+                             U.Clave == login.Clave
+                       select new LoginRespuesta
+                       {
+                           Usuario = U.Usuario,
+                           Autenticado = true,
+                           Token = token,
+                           Mensaje = "Usuario autenticado",
+                       };
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Models/LoginRespuesta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DBExamen3.Models
+{
+    public class LoginRespuesta
+    {
+        public string Usuario { get; set; }
+        public bool Autenticado { get; set; }
+        public string Token { get; set; }
+        public string Mensaje { get; set; }
+    }
+}
